10d916fbc344b03f05d101c23f83c3d79183a692
Artsiom <artkostm@gmail.com>
2019-09-29 11:21:28 +0300

    update tdlib builder for mac os

--- project/TdLib.scala
+++ project/TdLib.scala
@@ -8,35 +8,41 @@ object TdLib {
   case object MacOs extends Builder {
     override def build(): Path = {
 
-      remove.all(FS.Build)
-      makeDir(FS.Build)
+      if (!exists(FS.Lib) || list(FS.Lib).isEmpty) {
+        remove.all(FS.Build)
+        makeDir(FS.Build)
 
-      proc(
-        "cmake",
-        "-DCMAKE_BUILD_TYPE=Release",
-        "-DOPENSSL_ROOT_DIR=/usr/local/opt/openssl/",
-        "-DCMAKE_INSTALL_PREFIX:PATH=../example/java/td",
-        "-DTD_ENABLE_JNI=ON",
-        ".."
-      ).call(cwd = FS.Build, stdout = Inherit, stderr = Inherit)
+        proc(
+          "cmake",
+          "-DCMAKE_BUILD_TYPE=Release",
+          "-DOPENSSL_ROOT_DIR=/usr/local/opt/openssl/",
+          "-DCMAKE_INSTALL_PREFIX:PATH=../example/java/td",
+          "-DTD_ENABLE_JNI=ON",
+          ".."
+        ).call(cwd = FS.Build, stdout = Inherit, stderr = Inherit)
 
-      proc("cmake", "--build", ".", "--target", "install")
-        .call(cwd = FS.Build, stdout = Inherit, stderr = Inherit)
+        proc("cmake", "--build", ".", "--target", "install")
+          .call(cwd = FS.Build, stdout = Inherit, stderr = Inherit)
 
-      remove.all(FS.ExampleBuild)
-      makeDir(FS.ExampleBuild)
+        remove.all(FS.ExampleBuild)
+        makeDir(FS.ExampleBuild)
 
-      proc(
-        "cmake",
-        "-DCMAKE_BUILD_TYPE=Release",
-        "-DCMAKE_INSTALL_PREFIX:PATH=../../../tdlib",
-        "-DTd_DIR:PATH=" + (FS.Example / "td" / "lib" / "cmake" / "Td").toString(),
-        ".."
-      ).call(cwd = FS.ExampleBuild, stdout = Inherit, stderr = Inherit)
+        proc(
+          "cmake",
+          "-DCMAKE_BUILD_TYPE=Release",
+          "-DCMAKE_INSTALL_PREFIX:PATH=../../../tdlib",
+          "-DTd_DIR:PATH=" + (FS.Example / "td" / "lib" / "cmake" / "Td").toString(),
+          ".."
+        ).call(cwd = FS.ExampleBuild, stdout = Inherit, stderr = Inherit)
 
 
-      proc("cmake", "--build", ".", "--target", "install")
-        .call(cwd = FS.ExampleBuild, stdout = Inherit, stderr = Inherit)
+        proc("cmake", "--build", ".", "--target", "install")
+          .call(cwd = FS.ExampleBuild, stdout = Inherit, stderr = Inherit)
+
+        list(FS.TdLibBin).foreach(copy.matching {
+          case _ / file => FS.Lib / file
+        })
+      }
 
       FS.TdLibBin
     }
@@ -50,11 +56,13 @@ object TdLib {
   }
 
   object FS {
-    val Root         = pwd / "tdlib"
-    val Build        = Root / "build"
-    val Example      = Root / "example" / "java"
+    val Root         = pwd
+    val TdLib        = Root / "tdlib"
+    val Lib          = Root / "lib"
+    val Build        = TdLib / "build"
+    val Example      = TdLib / "example" / "java"
     val ExampleBuild = Example / "build"
-    val TdLibBin     = Root / "tdlib" / "bin"
+    val TdLibBin     = TdLib / "tdlib" / "bin"
   }
 
   trait Builder {
